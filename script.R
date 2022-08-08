6db0cd7d63d64a7658bfed77310c900f09b90d66
NewPhRbr <106818085+NewPhRbr@users.noreply.github.com>
2022-08-08 18:02:16 +0300

    Update script.R

--- script.R
+++ script.R
@@ -20,64 +20,71 @@ data <- data %>%
 
 # Переименовываем столбцы ---------------------------------------------------------
 data <- data %>%
-  select('ФИО пациента') %>%
-  t(.) %>%
-  str_split(., " ",simplify = TRUE) %>%
-  data_frame(.) %>%
-  mutate(middle_name = paste(.[,3],.[,4]), last_name = .[,1], first_name = .[,2]) %>%
-  select(first_name,last_name,middle_name) %>%
-  bind_cols(.,data)
-data['ФИО пациента'] <- NULL
-
+  mutate(name = str_split(`ФИО пациента`, " ", n = 3, simplify = T)) %>%
+  mutate(last_name = name[,1], first_name = name[,2], middle_name = name[,3])
+data$`ФИО пациента` <- NULL
 
 # Ищем пациентов, которые есть в базе ---------------------------------------------------------
 data_double <- data %>% 
-  select(first_name,last_name,middle_name)
-data_one <- data_redcap %>% 
-  select(record_id,first_name,last_name,middle_name) %>% 
-  left_join(data_double, 'last_name') %>% 
-  .[str_sub(.$first_name.x,1,1) == str_sub(.$first_name.y,1,1) & str_sub(.$middle_name.x,1,1) == str_sub(.$middle_name.y,1,1),] %>% 
-  .[!is.na(.$first_name.x),]
-data <- data %>% left_join(select(data_one, record_id, last_name, first_name = first_name.y, middle_name = middle_name.y))
+  select(first_name,
+         last_name,
+         middle_name,
+         sex = 'Пол',
+         birthdate = 'Дата.рожд.',
+         ds_dt = 'Дата установления первичного диагноза МБ',
+         relapse_dt = 'Дата рецидива (не событие)',
+         event = '1-е событие',
+         event_date = 'Дата события',
+         outcome = 'Исход',
+         outcome_date = 'Дата исхода') %>% 
+  mutate(pat_fio = paste(last_name,str_sub(`first_name`,1,1),gsub("-", "",birthdate), sep = "_"))
+data_one <- data_redcap %>%
+  select(record_id,
+         first_name,
+         last_name,
+         birthdate) %>%
+  mutate(pat_fio = paste(last_name,str_sub(`first_name`,1,1),gsub("-", "",birthdate), sep = "_")) %>% 
+  select(record_id,pat_fio)
+data_double <- data_double %>% left_join(data_one, by = 'pat_fio')
 
 # Фильтруем по присутствию в базе ---------------------------------------------------------
 
-data_base <- data %>% 
+data_base <- data_double %>% 
   filter(!is.na(.$record_id)) %>% 
   select(first_name,
          last_name,
          middle_name,
-         'Пол',
-         'Дата.рожд.',
-         'Дата установления первичного диагноза МБ' = 'Дата рецидива (не событие)',
-         '1-е событие',
-         'Дата события',
-         'Исход',
-         'Дата исхода') # Данные по пациентам, которые уже есть в базе
-data_not_base <- data %>% filter(is.na(.$record_id))
+         sex,
+         birthdate,
+         ds_dt = relapse_dt,
+         event,
+         event_date,
+         outcome,
+         outcome_date) # Данные по пациентам, которые уже есть в базе
+data_not_base <- data_double %>% filter(is.na(.$record_id))
 date_first <- data_not_base %>% 
   select(first_name,
          last_name,
          middle_name,
-         'Пол',
-         'Дата.рожд.',
-         'Дата установления первичного диагноза МБ',
-         '1-е событие',
-         'Дата события' = 'Дата рецидива (не событие)',
-         'Исход',
-         'Дата исхода') 
+         sex,
+         birthdate,
+         ds_dt,
+         event,
+         event_date = relapse_dt,
+         outcome,
+         outcome_date) 
 date_first[,'1-е событие'] = 'рецидив' # Данные по пациентам, которых в базе нет, рассматриваем первичный случай
 date_recidive <- data_not_base %>% 
   select(first_name,
          last_name,
          middle_name,
-         'Пол',
-         'Дата.рожд.',
-         'Дата установления первичного диагноза МБ' = 'Дата рецидива (не событие)',
-         '1-е событие',
-         'Дата события',
-         'Исход',
-         'Дата исхода') # Данные по пациентам, которых в базе нет, рассматриваем рецидив
+         sex,
+         birthdate,
+         ds_dt = relapse_dt,
+         event,
+         event_date,
+         outcome,
+         outcome_date) # Данные по пациентам, которых в базе нет, рассматриваем рецидив
 
 
 # Собираем таблицу ---------------------------------------------------------
@@ -86,19 +93,4 @@ data <- bind_rows(data_base,
           date_first,
           date_recidive)
 
-
-labels <- c(
-  'first_name' = 'first_name',
-  'last_name' = 'last_name',
-  'middle_name' = 'middle_name',
-  'Пол' = 'sex',
-  'Дата.рожд.' = 'birthdate',
-  'Дата установления первичного диагноза МБ' = 'ds_dt',
-  '1-е событие' = 'event',
-  'Дата события' = 'event_date',
-  'Исход' = 'outcome',
-  'Дата исхода' = 'outcome_date'
-)
-
-data <- data %>% rename_with(~ labels[.])
 write_csv(data, 'data_medull.csv')
