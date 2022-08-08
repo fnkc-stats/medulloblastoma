9d5ffa51d410b192acb48a33df1dfc17ea7004a4
NewPhRbr <106818085+NewPhRbr@users.noreply.github.com>
2022-08-08 18:10:07 +0300

    Update script.R

--- script.R
+++ script.R
@@ -8,27 +8,6 @@ data_redcap    <- read_csv('_DATA_2022-08-08_1325.csv')
 data <- readxl::read_xlsx('общая база.xlsx')
 data <- data %>% 
   select('ФИО пациента',
-         'Пол',
-         'Дата.рожд.',
-         'Дата установления первичного диагноза МБ',
-         'Дата рецидива (не событие)',
-         '1-е событие',
-         'Дата события',
-         'Исход',
-         'Дата исхода') 
-
-
-# Переименовываем столбцы ---------------------------------------------------------
-data <- data %>%
-  mutate(name = str_split(`ФИО пациента`, " ", n = 3, simplify = T)) %>%
-  mutate(last_name = name[,1], first_name = name[,2], middle_name = name[,3])
-data$`ФИО пациента` <- NULL
-
-# Ищем пациентов, которые есть в базе ---------------------------------------------------------
-data_double <- data %>% 
-  select(first_name,
-         last_name,
-         middle_name,
          sex = 'Пол',
          birthdate = 'Дата.рожд.',
          ds_dt = 'Дата установления первичного диагноза МБ',
@@ -36,7 +15,28 @@ data_double <- data %>%
          event = '1-е событие',
          event_date = 'Дата события',
          outcome = 'Исход',
-         outcome_date = 'Дата исхода') %>% 
+         outcome_date = 'Дата исхода') 
+
+
+# Переименовываем столбцы ---------------------------------------------------------
+data <- data %>%
+  mutate(name = str_split(`ФИО пациента`, " ", n = 3, simplify = T)) %>%
+  mutate(last_name = name[,1], first_name = name[,2], middle_name = name[,3])
+
+
+# Ищем пациентов, которые есть в базе ---------------------------------------------------------
+data_double <- data %>% 
+  select(first_name,
+         last_name,
+         middle_name,
+         sex,
+         birthdate,
+         ds_dt,
+         relapse_dt,
+         event,
+         event_date,
+         outcome,
+         outcome_date) %>% 
   mutate(pat_fio = paste(last_name,str_sub(`first_name`,1,1),gsub("-", "",birthdate), sep = "_"))
 data_one <- data_redcap %>%
   select(record_id,
@@ -50,7 +50,7 @@ data_double <- data_double %>% left_join(data_one, by = 'pat_fio')
 # Фильтруем по присутствию в базе ---------------------------------------------------------
 
 data_base <- data_double %>% 
-  filter(!is.na(.$record_id)) %>% 
+  filter(!is.na(record_id)) %>% 
   select(first_name,
          last_name,
          middle_name,
@@ -61,7 +61,7 @@ data_base <- data_double %>%
          event_date,
          outcome,
          outcome_date) # Данные по пациентам, которые уже есть в базе
-data_not_base <- data_double %>% filter(is.na(.$record_id))
+data_not_base <- data_double %>% filter(is.na(record_id))
 date_first <- data_not_base %>% 
   select(first_name,
          last_name,
