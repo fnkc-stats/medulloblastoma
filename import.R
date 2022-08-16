d90ed3530078e84f646a78c277cd4c563036cca4
NewPhRbr <106818085+NewPhRbr@users.noreply.github.com>
2022-08-16 11:00:19 +0300

    Update import.R

--- import.R
+++ import.R
@@ -4,7 +4,10 @@ library(lubridate)
 
 
 # Загрузка данных ---------------------------------------------------------
-data_redcap    <- read_csv('_DATA_2022-08-10_1116.csv')
+uri     <- "http://redcap.fccho-moscow.ru/api/"
+token   <- "9577A86D1421413C586D52B5AB35F5C4"
+data_redcap    <- REDCapR::redcap_read(redcap_uri=uri, token=token, raw_or_label = "label")$data
+
 
 data <- readxl::read_xlsx('общая база.xlsx')
 data <- data %>% 
@@ -93,7 +96,7 @@ data_first <- data_not_base %>%
 data_first <- data_first %>%  add_column(record_id = as.character(c((
   max(data_base$record_id)+1):(max(data_base$record_id) + 
                                  length(data_first$first_name)))))
-data_first[, 'event'] = 'рецидив' # Данные по пациентам, которых в базе нет, рассматриваем первичный случай
+data_first[, 'relapse'] = '1' # Данные по пациентам, которых в базе нет, рассматриваем первичный случай
 data_recidive <- data_not_base %>% 
   select(first_name,
          last_name,
@@ -101,37 +104,33 @@ data_recidive <- data_not_base %>%
          sex,
          birthdate,
          mgroup,
-         location = location_r,
          hist,
          ds_dt = relapse_dt,
          event,
          event_date,
          outcome,
          outcome_date)  %>%  
-  mutate(record_id = paste(data_first$record_id,'-R1',sep=''))# Данные по пациентам, которых в базе нет, рассматриваем рецидив
+  mutate(record_id = data_first$record_id)# Данные по пациентам, которых в базе нет, рассматриваем рецидив
 
-data_base <- data_base %>% mutate(record_id = paste(record_id,'-R1',sep=''))
+data_base_first <- data_base %>% mutate(record_id = paste(record_id,'-R1',sep=''))
+data_base <- data_base %>% mutate(record_id = paste(record_id))
 
 
 # Собираем таблицу ---------------------------------------------------------
 
-data <- bind_rows(data_base,
-                  data_recidive,
-                  data_first)
+data <- data_first
 
 length(t(unique(data[!is.na(data$record_id),"record_id"]))) == nrow(data[!is.na(data$record_id),])
-data[duplicated(data),] # смотрим, нет ли дупликатов
-
-
-# Перекодируем нужные столбцы в формат базы -----------------------------------------------
+data[duplicated(data),]
 
 data$sex <- recode(data$sex, 'М' = 'M', 'Ж' = 'F')
 data[str_starts(data$outcome, 'жив'), 'outcome'] = 'NO'
 data[str_starts(data$outcome, 'умер'), 'outcome'] = 'DTH'
 
 data <- data %>% 
-  add_column(relapse = .$event)
-data$relapse <- recode(data$relapse, 'рецидив' = '1', .default = '0')
+  mutate(relapse = coalesce(relapse, event))
+data$relapse <- recode(data$relapse, 'рецидив' = '1', '1' = '1' , .default = NA_character_)
+
 
 data$location___h <- as.integer(str_detect(data$location,'ЧМ'))
 data$location___4v <- as.integer(str_detect(data$location,'4Ж'))
@@ -142,5 +141,44 @@ data$hist <- recode(data$hist, 'КЛАСС' =	'CLASS',
                         'НОД' =	'NOD',
                         'ЭКСТ НОД' = 'EXTNOD',
                         'АНАПЛ' =	'LCA')
-                        
+
+data <- data %>% 
+    select(record_id,	first_name,	last_name,	middle_name,	sex,	birthdate,	mgroup,	hist,	ds_dt,	event,
+           event_date,	outcome,	outcome_date,	relapse,	location___h,	location___4v,	location___v)
+
+
+
+# для рецидивов --------------------------------------------
+
+data_recidive <- bind_rows(data_recidive,data_base)
+
+data_recidive$event <-  recode(data_recidive$event, 
+                               'нет событий' = '0',
+                               'смерть от осложнений лечения' = '2',
+                               'non-responder/прогрессия' = '5',
+                               'рецидив' = '3',
+                               'ранняя летальность' = '6')
+data_recidive$sex <- recode(data_recidive$sex, 'М' = 'M', 'Ж' = 'F')
+data_recidive[str_starts(data_recidive$outcome, 'жив'), 'outcome'] = 'NO'
+data_recidive[str_starts(data_recidive$outcome, 'умер'), 'outcome'] = 'DTH'
+
+data_recidive$hist <- recode(data_recidive$hist, 'КЛАСС' =	'CLASS',
+                    'НОД' =	'NOD',
+                    'ЭКСТ НОД' = 'EXTNOD',
+                    'АНАПЛ' =	'LCA')
+
+data_recidive$location___h <- as.integer(str_detect(data_recidive$location,'ЧМ'))
+data_recidive$location___4v <- as.integer(str_detect(data_recidive$location,'4Ж'))
+data_recidive$location___v <- as.integer(str_detect(data_recidive$location,'ГС'))
+data_recidive$location <- NULL
+
+
+
+# Записываем файлы -------------------------------------
+
 write_csv(data, 'data_medull.csv')
+openxlsx::write.xlsx(data, 'data_medull.xlsx')
+
+
+write_csv(data_recidive, 'data_medull_rel.csv')
+openxlsx::write.xlsx(data_recidive, 'data_medull_rel.xlsx')
