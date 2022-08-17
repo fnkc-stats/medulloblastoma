06a6288d145da3cf99c13d2b44da16e17498129f
NewPhRbr <106818085+NewPhRbr@users.noreply.github.com>
2022-08-17 14:27:20 +0300

    Update script_medulloblast_for_redcap.R

--- script_medulloblast_for_redcap.R
+++ script_medulloblast_for_redcap.R
@@ -19,11 +19,10 @@ data_rel <- data %>% #для вторых рецидивов
          outcome          = 'Исход',
          outcome_date     = 'Дата исхода',
          ds_dt            = 'Дата события',
-         relapse_type     = 'Локализация рецидива (событие)',
-         # rs         		=	'R-стадия',
+         location         = 'Локализация рецидива (событие)',
+         r_stage         	=	'Объем операции при рецидиве (не событие)',
          operation	    	= 'Операция при рецидиве (событие) да/нет',
          date         		=	'Дата операции при рецидиве (событие)',
-         # clinic_note		=	'Примечание',
          drt_is		        =	'ЛТ при рецидиве (событие) да/нет',
          drt			        =	'ЛТ при рецидиве (событие) первичная/повторная',
          drt_type	        =	'Вид ЛТ при рецидиве (событие) протоны/фотоны/РХ',
@@ -56,12 +55,12 @@ data <- data %>%
          outcome          = 'Исход',
          outcome_date     = 'Дата исхода',
          outcome_code     = 'Код исхода',
-         relapse_type     = 'Локализация рецидива (не событие)',
+         location         = 'Локализация рецидива (не событие)',
+         relapse_type     = 'Локализация рецидива (событие)',
          clinic_tr		    =	'Место проведения противорецидивного лечения',
-         # rs         			=	'R-стадия',
+         r_stage     			=	'Объем операции при рецидиве (событие)',
          operation	    	= 'Операция при рецидиве (не событие) да/нет',
          date         		=	'Дата операции при рецидиве (не событие)',
-         # clinic_note		  =	'Примечание',
          drt_is		        =	'ЛТ при рецидиве (не событие) да/нет',
          drt			        =	'ЛТ при рецидиве (не событие) первичная/повторная',
          drt_type	        =	'Вид ЛТ при рецидиве (не событие) фотоны/протоны/РХ',
@@ -124,64 +123,90 @@ data_recidive <- data_not_base %>%
 
 data_base_first <- data_base %>% mutate(record_id = paste(record_id,'-R1',sep=''))
 
-data_base_first$date <- as.character(data_base_first$date)
-data_recidive$date <- as.character(data_recidive$date)
-data_base_first$drt_dt <- as.character(data_base_first$drt_dt)
-data_recidive$drt_dt <- as.character(data_recidive$drt_dt)
-data_base_first$drt_end_stop <- as.character(data_base_first$drt_end_stop)
-data_recidive$drt_end_stop <- as.character(data_recidive$drt_end_stop)
-data_base_first$ct_start_dt <- as.character(data_base_first$ct_start_dt)
-data_recidive$ct_start_dt <- as.character(data_recidive$ct_start_dt)
 
 # Собираем таблицу для рецидивов --------------------------------------------
 
 data_recidive <- bind_rows(data_recidive, data_base_first)
+  
 
-data_recidive$event <-  recode(data_recidive$event, 
-                               'нет событий' = '0',
-                               'смерть от осложнений лечения' = '2',
-                               'non-responder/прогрессия' = '5',
-                               'рецидив' = '3',
-                               'ранняя летальность' = '6')
-data_recidive$sex <- recode(data_recidive$sex, 'М' = 'M', 'Ж' = 'F')
-data_recidive[str_starts(data_recidive$outcome, 'жив'), 'outcome'] = 'NO'
-data_recidive[str_starts(data_recidive$outcome, 'умер'), 'outcome'] = 'DTH'
+# Собираем таблицу для вторых рецидивов --------------------------------------------
 
+data_recidive_imp <- data_recidive %>% 
+  select(pat_fio,record_id)
+  data_rel <- data_rel %>% right_join(data_recidive_imp, by = 'pat_fio') %>% 
+  filter(event == 'рецидив') %>% 
+  mutate(name = str_split(record_id, "-", n = 2, simplify = T)) %>% 
+  mutate(record_id = paste(name[,1],'-R2', sep=''))
+  
+  data_recidive$pat_fio <- NULL
+  data_recidive$'ФИО пациента' <- NULL
+  data_recidive$name <- NULL
+  data_rel$pat_fio <- NULL
+  data_rel$'ФИО пациента' <- NULL
+  data_rel$name <- NULL
+  
 
-data_recidive <- data_recidive %>%
-  relocate(record_id,1)
+# делаем данные под файлы -------------------------------------
 
+data_recidive <- bind_rows(data_recidive, data_rel)
+  
+  data_recidive$event <-  recode(data_recidive$event, 
+                                 'нет событий' = '0',
+                                 'смерть от осложнений лечения' = '2',
+                                 'non-responder/прогрессия' = '5',
+                                 'рецидив' = '3',
+                                 'ранняя летальность' = '6')
+  data_recidive$sex <- recode(data_recidive$sex, 'М' = 'M', 'Ж' = 'F')
+  data_recidive[str_starts(data_recidive$outcome, 'жив'), 'outcome'] = 'NO'
+  data_recidive[str_starts(data_recidive$outcome, 'умер'), 'outcome'] = 'DTH'
+  
+  
+  data_recidive <- data_recidive %>%
+    relocate(record_id,1)
+  
+  data_recidive$location___loc <- as.integer(str_detect(data_recidive$location,'локальный'))
+  data_recidive$location___mts <- as.integer(str_detect(data_recidive$location,'метастатический'))
+  data_recidive$location___mix <- as.integer(str_detect(data_recidive$location,'смешанный'))
+  data_recidive$location <- NULL
+  
+  data_recidive$relapse_type <- recode(data_recidive$relapse_type,
+                                       'локальный' = 'LOC',
+                                       'метастатический' = 'MTS',
+                                       'смешанный' = 'MIX',
+                                       .default = '',
+                                       .missing = '')
+  
+  recode_is <- function(array_col){
+    array_col <- recode(array_col,
+                        'нет' = '0',
+                        'да' = '1',
+                        .default = '0')
+  }
+  
+  data_recidive$operation <- recode_is(data_recidive$operation)
+  data_recidive$drt_is <- recode_is(data_recidive$drt_is)
+  data_recidive$ct_is <- recode_is(data_recidive$ct_is)
+  data_recidive$ct_tgsk <- recode_is(data_recidive$ct_tgsk)
+  data_recidive$memmat_is <- recode_is(data_recidive$memmat_is)
+  
+  data_recidive$drt <- recode(data_recidive$drt, 'первичная' = '1',
+                              'повторная' = '2',
+                              .missing = 'NI')
+  NA_not_data_func <- function(str){
+    NA_not_data <- as.character(str_starts(data_recidive$drt_type, str)) 
+    NA_not_data <- as.logical(recode(NA_not_data, .missing = 'FALSE',
+                                     'TRUE' = 'TRUE', 'FALSE' = 'FALSE'))
+  }
+  
+  data_recidive[NA_not_data_func('фотоны'), 'drt_type'] = '1'
+  data_recidive[NA_not_data_func('протоны'), 'drt_type'] = '2'
+  data_recidive$drt_type[is.na(data_recidive$drt_type)] <- 'NI'
 
-data_recidive$relapse_type <- recode(data_recidive$relapse_type,
-                                     'локальный' = 'LOC',
-                                     'метастатический' = 'MTS',
-                                     'смешанный' = 'MIX')
-recode_is <- function(array_col){
-  array_col <- recode(array_col,
-         'нет' = '0',
-         'да' = '1',
-         .default = '0')
-}
+data_recidive$event_date <- as.character(data_recidive$event_date)
+data_recidive$event_date[is.na(data_recidive$event_date)] <- ''
 
-data_recidive$operation <- recode_is(data_recidive$operation)
-data_recidive$drt_is <- recode_is(data_recidive$drt_is)
-data_recidive$ct_is <- recode_is(data_recidive$ct_is)
-data_recidive$ct_tgsk <- recode_is(data_recidive$ct_tgsk)
-data_recidive$memmat_is <- recode_is(data_recidive$memmat_is)
-
-data_recidive$drt <- recode(data_recidive$drt, 'первичная' = '1',
-                                               'повторная' = '2',
-                                                .missing = 'NI')
-NA_not_data_func <- function(str){
-NA_not_data <- as.character(str_starts(data_recidive$drt_type, str)) 
-NA_not_data <- as.logical(recode(NA_not_data, .missing = 'FALSE',
-                      'TRUE' = 'TRUE', 'FALSE' = 'FALSE'))
-}
-
-data_recidive[NA_not_data_func('фотоны'), 'drt_type'] = '1'
-data_recidive[NA_not_data_func('протоны'), 'drt_type'] = '2'
-data_recidive$drt_type[is.na(data_recidive$drt_type)] <- 'NI'
-
+data_recidive[data_recidive$relapse_type != '', "relapse"] = '1'
+data_recidive[data_recidive$relapse_type == '', "relapse"] = 'NI'
 
 data_recidive$ct_start_dt <- as.character(data_recidive$ct_start_dt)
 data_recidive$ct_start_dt[is.na(data_recidive$ct_start_dt)] <- ''
@@ -201,99 +226,11 @@ data_recidive$memmat_start_dt <- as.character(data_recidive$memmat_start_dt)
 data_recidive$memmat_start_dt[is.na(data_recidive$memmat_start_dt)] <- ''
 data_recidive$memmat_end_dt <- as.character(data_recidive$memmat_end_dt)
 data_recidive$memmat_end_dt[is.na(data_recidive$memmat_end_dt)] <- ''
-  
+data_recidive$ct_end_dt <- as.character(data_recidive$ct_end_dt)
+data_recidive$ct_end_dt[is.na(data_recidive$ct_end_dt)] <- ''
 
-# Собираем таблицу для вторых рецидивов --------------------------------------------
-
-data_recidive_imp <- data_recidive %>% 
-  select(pat_fio,record_id)
-  data_rel <- data_rel %>% right_join(data_recidive_imp, by = 'pat_fio') %>% 
-  filter(event == 'рецидив') %>% 
-  mutate(name = str_split(record_id, "-", n = 2, simplify = T)) %>% 
-  mutate(record_id = paste(name[,1],'-R2', sep=''))
-  
-  data_rel$ct_start_dt <- as.character(data_rel$ct_start_dt)
-  data_rel$date <- as.character(data_rel$date)
-  data_rel$drt_dt <- as.character(data_rel$drt_dt)
-  data_rel$drt_dose <- as.character(data_rel$drt_dose)
-  data_rel$drt_bust_dose <- as.character(data_rel$drt_bust_dose)
-  data_rel$drt_end_stop <- as.character(data_rel$drt_end_stop)
-  data_rel$ct_num_blok <- as.character(data_rel$ct_num_blok)
-  data_rel$memmat_start_dt <- as.character(data_rel$memmat_start_dt)
-  data_rel$memmat_end_dt <- as.character(data_rel$memmat_end_dt)
-
-  data_rel$ct_start_dt[is.na(data_rel$ct_start_dt)] <- ''
-  data_rel$date[is.na(data_rel$date)] <- ''
-  data_rel$drt_dt[is.na(data_rel$drt_dt)] <- ''
-  data_rel$drt_dose[is.na(data_rel$drt_dose)] <- ''
-  data_rel$drt_bust_dose[is.na(data_rel$drt_bust_dose)] <- ''
-  data_rel$drt_end_stop[is.na(data_rel$drt_end_stop)] <- ''
-  data_rel$ct_num_blok[is.na(data_rel$ct_num_blok)] <- ''
-  data_rel$memmat_start_dt[is.na(data_rel$memmat_start_dt)] <- ''
-  data_rel$memmat_end_dt[is.na(data_rel$memmat_end_dt)] <- ''
-  
-  data_recidive$pat_fio <- NULL
-  data_recidive$'ФИО пациента' <- NULL
-  data_recidive$name <- NULL
-  data_rel$pat_fio <- NULL
-  data_rel$'ФИО пациента' <- NULL
-  data_rel$name <- NULL
-  
-  data_rel$event <-  recode(data_rel$event, 
-                                 'нет событий' = '0',
-                                 'смерть от осложнений лечения' = '2',
-                                 'non-responder/прогрессия' = '5',
-                                 'рецидив' = '3',
-                                 'ранняя летальность' = '6')
-  
-  data_rel$sex <- recode(data_rel$sex, 'М' = 'M', 'Ж' = 'F')
-  data_rel[str_starts(data_rel$outcome, 'жив'), 'outcome'] = 'NO'
-  data_rel[str_starts(data_rel$outcome, 'умер'), 'outcome'] = 'DTH'
-  
-  
-  data_rel <- data_rel %>%
-    relocate(record_id,1)
-  
-  
-  data_rel$relapse_type <- recode(data_rel$relapse_type,
-                                       'локальный' = 'LOC',
-                                       'метастатический' = 'MTS',
-                                       'смешанный' = 'MIX')
-  recode_is <- function(array_col){
-    array_col <- recode(array_col,
-                        'нет' = '0',
-                        'да' = '1',
-                        .default = '0')
-  }
-  
-  data_rel$operation <- recode_is(data_rel$operation)
-  data_rel$drt_is <- recode_is(data_rel$drt_is)
-  data_rel$ct_is <- recode_is(data_rel$ct_is)
-  data_rel$ct_tgsk <- recode_is(data_rel$ct_tgsk)
-  data_rel$memmat_is <- recode_is(data_rel$memmat_is)
-  
-  data_rel$drt <- recode(data_rel$drt, 'первичная' = '1',
-                              'повторная' = '2',
-                              .missing = 'NI')
-  NA_not_data_func <- function(str){
-    NA_not_data <- as.character(str_starts(data_rel$drt_type, str)) 
-    NA_not_data <- as.logical(recode(NA_not_data, .missing = 'FALSE',
-                                     'TRUE' = 'TRUE', 'FALSE' = 'FALSE'))
-  }
-  
-  data_rel[NA_not_data_func('фотоны'), 'drt_type'] = '1'
-  data_rel[NA_not_data_func('протоны'), 'drt_type'] = '2'
-  data_rel$drt_type[is.na(data_rel$drt_type)] <- 'NI'
-  
-  
 
 # Записываем файлы -------------------------------------
 
-data_recidive <- bind_rows(data_recidive, data_rel)
-
-data_recidive$event_date <- as.character(data_recidive$event_date)
-data_recidive$event_date[is.na(data_recidive$event_date)] <- ''
-  
-    
 write_csv(data_recidive, 'data_medull_rel.csv')
 openxlsx::write.xlsx(data_recidive, 'data_medull_rel.xlsx')
